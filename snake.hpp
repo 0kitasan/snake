f567900c4a044818811ed976a01f07cd3f96fcf8
0kitasan <292823441@qq.com>
2024-04-23 15:42:48 +0800

    renew readme

--- snake.hpp
+++ snake.hpp
@@ -7,16 +7,6 @@
 
 namespace snake {
 
-struct Node {
-  int x;
-  int y;
-  Node *next;
-};
-void AddEnd();
-void MoveToFirst();
-// 可以用链表存储蛇
-// 如果要更新蛇的位置，只需要把尾部元素挪到第一个即可
-
 class Snake {
 
 private:
@@ -85,4 +75,12 @@ int main() {
     return 0;
 }
 
+
+初始化
+绘制初始界面
+while(游戏未结束)
+监听输入
+蛇沿着原先的方向移动
+检测是否吃到食物
+检测是否越界
 */
